e380e861a6d9e9ce8e977034d52c3d500d338b80
Curtis Jensen <curtis.jensen@endurance.com>
2021-05-31 11:05:51 -0600

    Re-add react_native_app directory for troubleshooting git issue.

---
+++ react_native_app/react_native_app.baccc/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#773d93',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#45166a',
+    fontSize: '2em',
+  }
+});
+
+
